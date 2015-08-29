fcd60e8eb16f0e660235cb84a51a5cdebc771185
Chris Whong <chris@whong.org>
2015-08-29 13:50:33 -0400

    Added text pane and code to listen for drops

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-Sortable.create(sortable, {
+Sortable.create(itemList, {
   animation: 200,
   group: {
     name: 'a',
@@ -11,4 +11,12 @@ Sortable.create(sortable, {
 Sortable.create(sortable2, {
   animation: 200,
   group: "a"
+});
+
+var legend = $('#yourLegend').find('.cartodb-legend').parent();
+
+$('#html').find('.panel-body').text(legend.html());
+  
+$('#yourLegend').find('.cartodb-legend').on('drop',function(){
+  $('#html').find('.panel-body').text(legend.html());
 });
